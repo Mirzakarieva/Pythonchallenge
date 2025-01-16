86a54aac315f884dac41e40849d7e40e2272e82f
Mirzakarieva <144621217+Mirzakarieva@users.noreply.github.com>
2025-01-16 08:30:29 +0500

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,4 +1,9 @@
-urname = input("Enter your name:")
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the '+' sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+urname = input("Enter your name: ")
 
 print(len(urname))
 
