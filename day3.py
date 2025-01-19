024795aa4d4392a4b537a627098d2485e4665159
Mirzakarieva <144621217+Mirzakarieva@users.noreply.github.com>
2025-01-19 12:05:18 +0500

    Update day3.py

--- day3.py
+++ day3.py
@@ -6,7 +6,13 @@ else:
     print("This is an odd number")
 
 # BMI pro ;)
-
+# Write a program that interprets the Body Mass Index (BMI) based on a user's weight and height.
+# It should tell them the interpretation of their BMI based on the BMI value.
+# • Under 18.5 they are underweight
+# • Over 18.5 but below 25 they have a normal weight
+# • Over 25 but below 30 they are overweight
+# • Over 30 but below 35 they are obese
+# • Above 35 they are clinically obese.
 
 height = float(input("Enter your height in m: "))
 weight = float(input("Enter your weight in kg: "))
@@ -14,18 +20,20 @@ weight = float(input("Enter your weight in kg: "))
 bmi = weight / height ** 2
 
 if bmi < 18.5:
-    message = " underweighted"
+    message = " underweight"
 elif bmi < 25:
     message = " normal weighted"
 elif bmi < 30:
-    message = " overweighted"
+    message = " overweight"
 elif bmi < 35:
-    message = " obesed"
+    message = " obese"
 else:
-    message = " clinically obesed"
+    message = " clinically obese"
 print(f"Your BMT is {round(bmi)}, you are {message}.")
 
 # leap year
+# Write a program that works out whether if a given year is a leap year.
+# A normal year has 365 days, leap years have 366, with an extra day in February.
 year = int(input("Which year do you want to check? "))
 
 if year % 400 == 0:
@@ -40,7 +48,15 @@ else:
 print(f"The year {year} is {result} year")
 
 # Pizza
-
+# Congratulations, you've got a job at Python Pizza.
+# Your first job is to build an automatic pizza order program.
+# Based on a user's order, work out their final bill.
+# Small Pizza: $15
+# Medium Pizza: $20
+# Large Pizza: $25
+# Pepperoni for Small Pizza: +$2
+# Pepperoni for Medium or Large Pizza: +$3
+# Extra cheese for any size pizza: + $1
 
 print("Welcome to Python Pizza Deliveries!")
 size = input("What size pizza do you want? S, M, or L ")
@@ -60,12 +76,16 @@ else:
     if add_pepperoni == "Y":
         bill += 3
 
-# the other way to pepperoni
+# Other 2 ways for pepperoni:
+# 1:
 # if add_pepperoni == "Y":
 #     if size == "S":
 #         bill += 2
 #     else:
 #         bill += 3
+# 2:
+# if add_pepperoni == "Y":
+#     bill += 3 if size != "S" else 2
 
 
 if extra_cheese == "Y":
@@ -74,13 +94,22 @@ if extra_cheese == "Y":
 print(f"Your final bill is: ${bill}.")
 
 # Love calculator
+# To work out the love score between two people:
+# Take both people's names and check for the number of times the letters in the word TRUE occurs.
+# Then check for the number of times the letters in the word LOVE occurs. Then combine these numbers to make a 2-digit number.
+# For Love Scores less than 10 or greater than 90, the message should be:
+# "Your score is x, you go together like coke and mentos."
+# For Love Scores between 40 and 50, the message should be:
+# "Your score is y, you are alright together.
+# Otherwise, the message will just be their score. e.g.:
+# "Your score is z.
 print("Welcome to the Love Calculator!")
 name1 = input("What is your name? \n")
 name2 = input("What is his/her name? \n")
 
 name1 = name1.lower()
 name2 = name2.lower()
-l = name1.count("l") + name2.count("l")
+L = name1.count("l") + name2.count("l")
 o = name1.count("o") + name2.count("o")
 v = name1.count("v") + name2.count("v")
 e = name1.count("e") + name2.count("e")
@@ -89,11 +118,11 @@ t = name1.count("t") + name2.count("t")
 r = name1.count("r") + name2.count("r")
 u = name1.count("u") + name2.count("u")
 
-result = (t + r + u + e) * 10 + (l + o + v + e)
+result = (t + r + u + e) * 10 + (L + o + v + e)
 
 if result <= 10 or result >= 90:
     message = "you go together like coke and mentos."
-elif result >= 40 and result <= 50:
+elif 40 >= result <= 50:
     message = "you are alright together."
 else:
     message = ""
@@ -101,12 +130,37 @@ else:
 print(f"Yor score is {result},", message)
 
 # Treasure something
+print('''
+
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."/` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/[TomekK]
+*******************************************************************************
+''')
 print("Welcome to Treasure Island. Your mission is to find the treasure.")
-direction = input("left or right?")
+direction = input('You\'re at a crossroad,crossroad, where do you want to go? Type "left" or "right".').lower()
 if direction == "left":
-    action = input("swim or wait?")
+    action = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat.'
+                   ' Type "swim" to swim across.').lower()
     if action == "wait":
-        door = input("Which door? red, blue or yellow?")
+        door = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue."
+                     "Which colour do you choose?").lower()
         if door == "red":
             print("Burned by fire. Game Over.")
         elif door == "blue":
